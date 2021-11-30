82b01c69b7e2acd388f6547d582ec8704369d1c6
gzhang71 <arthur.zhanggq@gmail.com>
2021-11-29 21:28:31 -0500

    Create 426. Convert Binary Search Tree to Sorted Doubly Linked List.py

---
+++ answer/426. Convert Binary Search Tree to Sorted Doubly Linked List.py
@@ -0,0 +1,35 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+"""
+
+
+class Solution:
+    def treeToDoublyList(self, root: 'Node') -> 'Node':
+        if not root:
+            return root
+        stack = []
+        curr = root
+        last = Node(0)
+        head = last
+        while stack or curr:
+            while curr:
+                stack.append(curr)
+                curr = curr.left
+            # print(stack)
+            curr = stack.pop()
+            value = head.right.val if head.right else None
+
+            last.right = curr
+            curr.left = last
+            last = curr
+            curr = curr.right
+
+        curr = head.right
+        last.right = curr
+        curr.left = last
+        return curr
